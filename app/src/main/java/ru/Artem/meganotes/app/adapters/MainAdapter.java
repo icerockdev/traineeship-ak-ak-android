97dd80d012216c8330e9a47c20222096a69b8edd
Artem <kharitonov1995@gmail.com>
2016-05-03 23:48:29 +0600

    Удалил контекст из конструктора. Удалил картинку

--- app/src/main/java/ru/Artem/meganotes/app/adapters/MainAdapter.java
+++ app/src/main/java/ru/Artem/meganotes/app/adapters/MainAdapter.java
@@ -29,14 +29,12 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.NoteViewHolder
     private OnItemClickListener mOnItemClickListener;
     private OnLongItemClickListener mOnLongItemClickListener;
     private List<ModelNote> mNotesList;
-    private Context mContext;
 
     public class NoteViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener,
             View.OnLongClickListener {
 
         private TextView nameNote;
         private TextView lastUpdateNote;
-        private ImageView imgNote;
 
         public NoteViewHolder(View itemView) {
 
@@ -44,7 +42,6 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.NoteViewHolder
 
             nameNote = (TextView)itemView.findViewById(R.id.nameNote);
             lastUpdateNote = (TextView)itemView.findViewById(R.id.lastUpdateNote);
-            imgNote = (ImageView)itemView.findViewById(R.id.imgNote);
 
             itemView.setOnClickListener(this);
             itemView.setOnLongClickListener(this);
@@ -69,9 +66,8 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.NoteViewHolder
         }
     }
 
-    public MainAdapter(List<ModelNote> modelNotes, Context mainActivityContext){
+    public MainAdapter(List<ModelNote> modelNotes){
         this.mNotesList = modelNotes;
-        this.mContext = mainActivityContext;
     }
 
     public interface OnItemClickListener {
@@ -103,99 +99,7 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.NoteViewHolder
 
     @Override
     public void onBindViewHolder(NoteViewHolder noteViewHolder, int i) {
-
         noteViewHolder.nameNote.setText(mNotesList.get(i).getNameNote());
         noteViewHolder.lastUpdateNote.setText(mNotesList.get(i).getLastUpdateNote());
-
-        if(!mNotesList.get(i).getPathImg().isEmpty() && !mNotesList.get(i).getPathImg().equals(DetailedActivity.DELETE_IMG)) {
-            fetchDrawableOnThread(noteViewHolder.imgNote, i);
-        } else if (mNotesList.get(i).getPathImg().equals(DetailedActivity.DELETE_IMG)) { // плохое решение, спросить!
-            noteViewHolder.imgNote.setImageDrawable(null);
-        }
-    }
-
-    public void fetchDrawableOnThread (final ImageView imageView, final int i) {
-
-        if (mNotesList.get(i).getBitmap() != null) {
-            imageView.setImageBitmap(mNotesList.get(i).getBitmap());
-        }
-
-        final android.os.Handler handler = new android.os.Handler() {
-            @Override
-            public void handleMessage(Message msg) {
-
-                final Bitmap image = (Bitmap) msg.obj;
-                imageView.setImageBitmap(image);
-
-            }
-        };
-
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    mNotesList.get(i).setBitmap(scaleImg(i));
-                    final Message message = handler.obtainMessage(1, mNotesList.get(i).getBitmap());
-                    handler.sendMessage(message);
-                } catch (IOException ex) {
-
-                }
-            }
-        });
-        thread.start();
-    }
-
-    public Bitmap scaleImg(int position) throws IOException {
-
-        InputStream inputStream;
-        InputStream inputStreamScale;
-        inputStream = mContext.getContentResolver()
-                .openInputStream(Uri.parse(mNotesList.get(position).getPathImg()));
-
-        BitmapFactory.Options onlyBoundsOptions = new BitmapFactory.Options();
-        onlyBoundsOptions.inJustDecodeBounds = true;
-
-        BitmapFactory.decodeStream(inputStream, null, onlyBoundsOptions);
-
-        onlyBoundsOptions.inSampleSize = calculateInSampleSize(onlyBoundsOptions, 128, 90);
-        onlyBoundsOptions.inJustDecodeBounds = false;
-
-        inputStreamScale = mContext.getContentResolver()
-                .openInputStream(Uri.parse(mNotesList.get(position).getPathImg()));
-
-        return BitmapFactory.decodeStream(inputStreamScale, null, onlyBoundsOptions);
-    }
-
-    @Override
-    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
-        super.onAttachedToRecyclerView(recyclerView);
-    }
-
-    public static int calculateInSampleSize(
-            BitmapFactory.Options options, int reqWidth, int reqHeight) {
-
-        int height = options.outHeight;
-        int width = options.outWidth;
-        int inSampleSize = 1;
-        int tmp;
-
-        if (width > height){
-            tmp = height;
-            height = width;
-            width = tmp;
-        }
-
-        if (height > reqHeight || width > reqWidth) {
-
-            final int halfHeight = height / 2;
-            final int halfWidth = width / 2;
-
-            while ((halfHeight / inSampleSize) > reqHeight
-                    && (halfWidth / inSampleSize) > reqWidth) {
-                inSampleSize *= 2;
-            }
-        }
-
-        return inSampleSize;
     }
 }
